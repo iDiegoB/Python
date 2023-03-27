0ddbe92d0edf975ad288417b51f6f0dcf27ebc0e
iDiegoB <diegobarrabarrera@gmail.com>
2023-03-27 16:56:49 -0300

    "a"

---
+++ Manejo_de_Archivos/tortuga.py
@@ -0,0 +1,17 @@
+import turtle
+terrel = turtle.Turtle()
+terrel2 = turtle.Turtle()
+
+terrel.forward(200)
+terrel.right(90)
+terrel.forward(200)
+terrel.right(90)
+terrel.forward(200)
+terrel.right(90)
+terrel.forward(200)
+terrel.right(90)
+
+for item in range(4):
+ terrel2.forward(150)
+ terrel2.right(90)
+turtle.Screen().exitonclick()
